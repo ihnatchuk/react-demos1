b619917cc71085ab0e04ddac27471bf6ce6d05e3
ihnatchuk <ihnatchukpp@gmail.com>
2022-10-24 00:55:40 +0300

    increment decrement (useState)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+    let [counter, setCounter] = useState(0);
+    const increment=()=>{
+        setCounter(++counter);
+    }
+    const decrement=()=>{
+        setCounter(--counter);
+    }
+    return (
+        <div className={"wrap"}>
+            <h2>{counter}</h2>
+            <div className="buttons">
+                <div>
+                    <button onClick={increment}>Inc</button>
+                </div>
+                <div>
+                    <button onClick={decrement}>Dec</button>
+                </div>
+            </div>
+        </div>
 
-    </div>
-  );
+    );
 }
-
 export default App;
