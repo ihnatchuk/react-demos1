7f91978b10cedaefb98dffe1de3f9d6dbad0c2d4
ihnatchuk <ihnatchukpp@gmail.com>
2022-10-24 18:18:27 +0300

    fetch users, using useState, useEffect.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import {findAllByDisplayValue} from "@testing-library/react";
+import React, {useEffect, useState} from 'react';
 
 function App() {
+    let [users,setUsers]=useState([]);
+    useEffect(()=>{
+      fetch('https://jsonplaceholder.typicode.com/users')
+          .then(value => value.json())
+          .then(value =>{
+            setUsers(value);
+          } )
+    },[])
   return (
-    <div className="App">
+    <div>
+      {
+        users.map(value => <div>
+          <h2>{value.id}. {value.name}</h2>
+          <p>{value.email}</p>
+        </div>
+        )
+      }
 
     </div>
   );
